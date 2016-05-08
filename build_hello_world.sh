81046e4da9277b74924a1a8e4f0db1e9ddd59905
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-08 19:37:12 +0200

    Can build 'Hello World' and test for success locally

---
+++ build_hello_world.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+myexe="/home/richel/Programs/processing-3.0.1/processing-java"
+input_folder="hello_world"
+output_folder=$input_folder"_output"
+
+if [ ! -e $myexe ]
+then
+  echo "Executable '"$myexe"' not found, line "$LINENO
+  exit 1
+fi
+
+if [ ! -e $input_folder ]
+then
+  echo "Input folder '"$input_folder"' not found, line "$LINENO
+  exit 1
+fi
+
+if [ -e $output_folder ]
+then
+  echo "Output folder '"$output_folder"' found, deleting it"
+  rm -rf $output_folder
+fi
+
+if [ -e $output_folder ]
+then
+  echo "Folder '"$output_folder"' could not be deleted, line "$LINENO
+  exit 1
+fi
+
+$myexe --sketch=$input_folder --output=hello_world_output --build
+
+if [ -e hello_world_output/hello_world.class ]
+then
+  echo "Success"
+  exit 0
+else
+  echo "Fail"
+  exit 1
+fi
